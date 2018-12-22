0870061373a55232b4448eb7b1fa0cb8528cc2e5
Roman Chomik <romanchom@gmail.com>
2018-12-22 21:00:53 +0100

    WIP flatbuffer protocol

--- Makefile
+++
@@ -1,5 +0,0 @@
-PROJECT_NAME := LunaEsp32
-
-CXXFLAGS := -std=c++14
-
-include $(IDF_PATH)/make/project.mk
--- main/component.mk
+++
@@ -1,3 +0,0 @@
-COMPONENT_EMBED_TXTFILES += certificates/ca_cert.pem
-COMPONENT_EMBED_TXTFILES += certificates/my_cert.pem
-COMPONENT_EMBED_TXTFILES += certificates/my_key.pem
