c2681a1cb54966708f54af143b247ce671202202
Roman Chomik <romanchom@gmail.com>
2019-05-16 23:08:20 +0200

    Disabled WiFi power saving to improve responsiveness and fix jitter.

--- main/main.cpp
+++ main/main.cpp
@@ -63,6 +63,7 @@ static void initializeWiFi()
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "WiFi initialized.");
