a955fff682713aee4dadfacc643a2653ef979b56
Roman Chomik <romanchom@gmail.com>
2019-05-19 19:52:03 +0200

    Preliminary atx support.

--- main/main.cpp
+++ main/main.cpp
@@ -3,6 +3,8 @@
 #include <esp_system.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
+#include <driver/gpio.h>
+#include <driver/ledc.h>
 
 #include <cstring>
 #include <memory>
@@ -90,44 +92,71 @@ static void wifiDisconnected()
     lunaNetworkManager->disable();
 }
 
-std::shared_ptr<luna::esp32::StrandWS2812> makePhysicalStrand()
+std::unique_ptr<luna::esp32::StrandWS2812> makeLeftStrand()
 {
     luna::esp32::WS2812Configuration config;
-    config.pixelCount = 100;
-    config.begin = {-1.0f, -1.0f};
+    config.pixelCount = 120;
+    config.begin = {-1.0f, -1.2f};
     config.end = {-1.0f, 1.0f};
-    config.gpioPin = luna::esp32::output5;
+    config.gpioPin = 26;
 
-    return std::make_shared<luna::esp32::StrandWS2812>(config);
+    return std::make_unique<luna::esp32::StrandWS2812>(config);
 }
 
-std::unique_ptr<luna::esp32::Strand> makeRightStrand(std::shared_ptr<luna::esp32::StrandWS2812> child)
+std::unique_ptr<luna::esp32::StrandWS2812> makeRightStrand()
 {
-    luna::esp32::StrandConfiguration config;
-    config.pixelCount = 36;
-    config.begin = {1.5f, -3.0f};
-    config.end = {1.5f, 2.0f};
+    luna::esp32::WS2812Configuration config;
+    config.pixelCount = 120;
+    config.begin = {1.0f, -1.2f};
+    config.end = {1.0f, 1.0f};
+    config.gpioPin = 27;
 
-    return std::make_unique<luna::esp32::StrandView>(std::move(child), config, 0);
-}
-
-std::unique_ptr<luna::esp32::Strand> makeTopStrand(std::shared_ptr<luna::esp32::StrandWS2812> child)
-{
-    luna::esp32::StrandConfiguration config;
-    config.pixelCount = 57;
-    config.begin = {1.5f, 2.0f};
-    config.end = {-4.0f, 2.0f};
-
-    return std::make_unique<luna::esp32::StrandView>(std::move(child), config, 36);
+    return std::make_unique<luna::esp32::StrandWS2812>(config);
 }
 
 static void configureHardware()
 {
-    auto physicalStrand = makePhysicalStrand();
-
     auto & strands = controller.strands();
-    strands.emplace_back(makeRightStrand(physicalStrand));
-    strands.emplace_back(makeTopStrand(physicalStrand));
+    strands.emplace_back(makeLeftStrand());
+    strands.emplace_back(makeRightStrand());
+
+    gpio_config_t io_conf;
+    io_conf.intr_type = gpio_int_type_t(GPIO_PIN_INTR_DISABLE);
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = 1 << 14;
+    io_conf.pull_down_en = gpio_pulldown_t(0);
+    io_conf.pull_up_en = gpio_pullup_t(0);
+    gpio_config(&io_conf);
+
+    gpio_set_level(gpio_num_t(14), 0);
+
+
+    ledc_timer_config_t ledc_timer;
+    ledc_timer.duty_resolution = ledc_timer_bit_t(8);
+    ledc_timer.freq_hz = 40000;
+    ledc_timer.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_timer.timer_num = LEDC_TIMER_0;
+    ledc_timer_config(&ledc_timer);
+
+    ledc_channel_config_t ledc_channel;
+    ledc_channel.channel = LEDC_CHANNEL_0;
+    ledc_channel.duty = 0;
+    ledc_channel.gpio_num = gpio_num_t(32);
+    ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel.hpoint = 0;
+    ledc_channel.timer_sel = LEDC_TIMER_0;
+    ledc_channel_config(&ledc_channel);
+
+
+
+
+    controller.onEnabled([](bool enabled) {
+        gpio_set_level(gpio_num_t(14), enabled ? 1 : 0);
+        ESP_LOGI(TAG, "Enabled. %d", (enabled ? 1 : 0));
+
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, enabled ? 90 : 0);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+    });
 }
 
 static void initializeLuna()
