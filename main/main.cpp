09ed686c49133b8b1b7dacaeb8452627ddfd8544
Roman Chomik <romanchom@gmail.com>
2020-01-27 20:01:32 +0100

    Updated library version. Added sensible build defaults.

--- main/main.cpp
+++ main/main.cpp
@@ -1,43 +1,53 @@
 #include "Certificates.hpp"
 
-#include <luna/Main.hpp>
-#include <luna/NetworkManager.hpp>
-#include <luna/HardwareController.hpp>
+#include <luna/EventLoop.hpp>
+
+#include <luna/ConstantEffect.hpp>
+#include <luna/FlameEffect.hpp>
+#include <luna/PlasmaEffect.hpp>
+
+#include <luna/EffectsPlugin.hpp>
+#include <luna/MqttPlugin.hpp>
+#include <luna/UpdatePlugin.hpp>
+#include <luna/PersistencyPlugin.hpp>
+#include <luna/RealtimePlugin.hpp>
+
+#include <luna/Infrared.hpp>
+#include <luna/Ir40ButtonRemote.hpp>
+
+#include <luna/Luna.hpp>
+
+#include <luna/Device.hpp>
 #include <luna/StrandWS281x.hpp>
+#include <luna/ATX.hpp>
+#include <luna/PWMLight.hpp>
+#include <luna/WS281xMeter.hpp>
 
 #include <esp_log.h>
 
 static char const TAG[] = "App";
 
 using namespace luna;
-using namespace std::literals;
 
-struct RoomLightController : public HardwareController
+struct Lamp : Device
 {
-    explicit RoomLightController() :
-        physicalStrand(
-            14,
-            93
+    explicit Lamp() :
+        mPhysicalStrand(
+            32,
+            100
         ),
-        rightStrand(
-            {{1.5f, -3.0f, 0.0f}, {1.5f, 2.0f, 0.0f}},
-            &physicalStrand,
-            36,
+        mStrand(
+            {{-3.0f, 2.0f, 0.0f}, {2.0f, 2.0f, 0.0f}},
+            &mPhysicalStrand,
+            100,
             0
-        ),
-        topStrand(
-            {{1.5f, 2.0f, 0.0f}, {-4.0f, 2.0f, 0.0f}},
-            &physicalStrand,
-            57,
-            36
         )
     {}
 
     std::vector<Strand *> strands() final
     {
         return {
-            &rightStrand,
-            &topStrand
+            &mStrand
         };
     }
 
@@ -46,31 +56,46 @@ struct RoomLightController : public HardwareController
 
     void update() final
     {
-        physicalStrand.send();
+        mPhysicalStrand.send();
     }
 private:
-    WS281xDriver physicalStrand;
-    StrandWS2811 rightStrand;
-    StrandWS2811 topStrand;
-};
-
-Configuration const config{
-    .wifi{
-        .ssid = CONFIG_ESP_WIFI_SSID,
-        .password = CONFIG_ESP_WIFI_PASSWORD
-    },
-    .network{
-        .name = "pokoj"sv,
-        .mqttAddress = "mqtts://192.168.0.233:8883"sv,
-        .tls{
-            .ownKey{myKey, size_t(myKeyEnd - myKey)},
-            .ownCertificate{myCert, size_t(myCertEnd - myCert)},
-            .caCertificate{caCert, size_t(caCertEnd - caCert)},
-        },
-    },
+    WS281xDriver mPhysicalStrand;
+    StrandWS2811 mStrand;
 };
 
 extern "C" void app_main()
 {
-    new Main(config, new RoomLightController());
+    EventLoop mainLoop;
+
+    Lamp hardware;
+
+    ConstantEffect light("light");
+    FlameEffect flame("flame");
+    PlasmaEffect plasma("plasma");
+
+    EffectPlugin effects(&mainLoop, {&light, &flame, &plasma});
+    PersistencyPlugin persistency(&effects.effectEngine());
+    MqttPlugin mqttPlugin("pokoj", "mqtt://192.168.1.100", &mainLoop, &effects.effectEngine());
+    UpdatePlugin update;
+    RealtimePlugin realtime("pokoj", &hardware);
+
+    Ir40ButtonRemote demux(&effects.effectEngine(), &light);
+    Infrared ir(&mainLoop, 12, &demux);
+
+    LunaConfiguration config{
+        .plugins = {&effects, &mqttPlugin, &update, &realtime},
+        .hardware = &hardware,
+        .wifiCredentials{
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .password = CONFIG_ESP_WIFI_PASSWORD
+        },
+        .tlsCredentials{
+            .ownKey{myKey, size_t(myKeyEnd - myKey)},
+            .ownCertificate{myCert, size_t(myCertEnd - myCert)},
+            .caCertificate{caCert, size_t(caCertEnd - caCert)},
+        }
+    };
+
+    Luna lun(&mainLoop, &config);
+    mainLoop.execute();
 }
