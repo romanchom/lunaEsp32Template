9438f78a5e6c82580e9af0a5ccbf90f6edc4698d
Roman Chomik <romanchom@gmail.com>
2022-01-08 14:30:59 +0100

    Overhaul needed for esp8266 support

--- examples/esp8266_light_bulb/main/main.cpp
+++ examples/esp8266_light_bulb/main/main.cpp
@@ -1,20 +1,17 @@
-#include "Certificates.hpp"
-
-#include <luna/EventLoop.hpp>
-
 #include <luna/ConstantEffect.hpp>
 #include <luna/FlameEffect.hpp>
 #include <luna/PlasmaEffect.hpp>
 
 #include <luna/EffectPlugin.hpp>
 #include <luna/MqttPlugin.hpp>
-#include <luna/UpdatePlugin.hpp>
 #include <luna/PersistencyPlugin.hpp>
+#include <luna/UpdatePlugin.hpp>
 
 #include <luna/Luna.hpp>
 
 #include <luna/Device.hpp>
-#include <luna/StrandWS281x.hpp>
+#include <luna/PWMLight.hpp>
+#include <luna/SoftPWM.hpp>
 
 #include <esp_log.h>
 
@@ -22,32 +19,36 @@ static char const TAG[] = "App";
 
 using namespace luna;
 
+enum {
+    GPIO_RED = 4,
+    GPIO_GREEN = 12,
+    GPIO_BLUE = 14,
+    GPIO_COOL_WHITE = 5,
+    GPIO_WARM_WHITE = 13,
+};
+
 struct Lamp : Device
 {
     explicit Lamp() :
-        mPhysicalStrand(
-            14,
-            94
-        ),
-        mStrandTop(
-            {{1.0f, 1.0f, 0.0f}, {-1.9f, 1.0f, 0.0f}},
-            &mPhysicalStrand,
-            58,
-            36
-        ),
-        mStrandSide(
-            {{1.0f, -0.9f, 0.0f}, {0.0f, 1.0f, 0.0f}},
-            &mPhysicalStrand,
-            36,
-            0
-        )
+        mPWM({
+            GPIO_RED,
+            GPIO_GREEN,
+            GPIO_BLUE,
+            GPIO_COOL_WHITE,
+            GPIO_WARM_WHITE,
+        }),
+        mLight({{0, 0, 0}, {0, 0, 0}}, prism::sRGB(), {
+            {mPWM.getChannel(0), PWMLight::Red},
+            {mPWM.getChannel(1), PWMLight::Green},
+            {mPWM.getChannel(2), PWMLight::Blue},
+            {mPWM.getChannel(3), PWMLight::White},
+        })
     {}
 
     std::vector<Strand *> strands() final
     {
         return {
-            &mStrandTop,
-            &mStrandSide,
+            &mLight,
         };
     }
 
@@ -56,12 +57,11 @@ struct Lamp : Device
 
     void update() final
     {
-        mPhysicalStrand.send();
+        mPWM.commit();
     }
 private:
-    WS281xDriver mPhysicalStrand;
-    StrandWS2811 mStrandTop;
-    StrandWS2811 mStrandSide;
+    SoftPWM mPWM;
+    PWMLight mLight;
 };
 
 extern "C" void app_main()
@@ -78,20 +78,15 @@ extern "C" void app_main()
         {"plasma",&plasma}
     });
     PersistencyPlugin persistency(&effects);
-    MqttPlugin mqttPlugin("bedroom", "mqtt://192.168.4.8", &effects, 255.0f);
+    MqttPlugin mqttPlugin("lamp", "mqtt://192.168.4.8", &effects, 255.0f);
     UpdatePlugin update;
 
     LunaConfiguration config{
-        .plugins = {&effects, &mqttPlugin, &update,},
+        .plugins = {&persistency, &effects, &mqttPlugin, &update},
         .device = &device,
         .wifiCredentials{
             .ssid = CONFIG_ESP_WIFI_SSID,
             .password = CONFIG_ESP_WIFI_PASSWORD
-        },
-        .tlsCredentials{
-            .ownKey{myKey, size_t(myKeyEnd - myKey)},
-            .ownCertificate{myCert, size_t(myCertEnd - myCert)},
-            .caCertificate{caCert, size_t(caCertEnd - caCert)},
         }
     };
 
